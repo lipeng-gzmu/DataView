91b234306b895456e467aa7e21eff7040c5d98bd
lipeng-gzmu <1826427954@qq.com>
2021-05-14 16:12:40 +0800

    lipeng

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var hand = $("#hand")
+$(hand).onload(fadeview())
+
+function fadeview(){
+//	$("#hand").fadeOut(1000);
+   
+      $(hand).fadeToggle(1500)
+	  $(hand).fadeToggle(1500)
+    fadeview()
+	
+}
+	
+
