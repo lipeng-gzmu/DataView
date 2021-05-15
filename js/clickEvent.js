23f136e41c304337e18a4aad8e42c9d19c252414
lipeng-gzmu <1826427954@qq.com>
2021-05-15 11:37:55 +0800

    lipeng

--- js/clickEvent.js
+++
@@ -1,3 +0,0 @@
-$("#catalogue1").click(function(){
-	window.open ('page.html', 'newwindow', 'height=550, width=1200, top=0, left=0, toolbar=no, menubar=no, scrollbars=no,resizable=no,location=yes, status=no')
-})
